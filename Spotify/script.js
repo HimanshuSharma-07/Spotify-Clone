c511fa596ece960695c14f145e67f1bbe58f6132
HimanshuSharma-07 <sharmahimanshu82194@gmail.com>
2025-09-22 23:37:53 +0530

    Add files via upload

--- Spotify/script.js
+++ Spotify/script.js
@@ -1,5 +1,20 @@
 console.log("Lets write JavaScript");
 
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+
 async function getSongs(){
 
     let a = await fetch("http://127.0.0.1:5500/songs/")
@@ -25,12 +40,22 @@ async function getSongs(){
     
 } 
 
-async function main(){
+let currTrack = new Audio()
 
+const playMusic = (track)=>{
+    // let audio = new Audio("songs/" + track)
+    currTrack.src = "songs/" + track
+    currTrack.play()
+    play.src = "img/pause.svg"
+    document.querySelector(".songinformation").innerHTML = track
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+}
+
+async function main(){
     // Get the list of all songs
     let songs = await getSongs()
-    console.log(songs);
-
+    
+    // Show all the songs in the playlist
     let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0]
     for(const song of songs) {
         songUl.innerHTML = songUl.innerHTML + `<li>
@@ -40,29 +65,47 @@ async function main(){
                                 
                             </div>
                             <div class="playnow">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
-  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-  <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
-</svg>
+                            
 
-                            </div>
-        </li>`
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                                <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
+                                </svg>
+
+                            </div> </li>`
     }
 
-    // Play the first song
-    let audio = new Audio(songs[0]); // path to your file
-    // audio.play();
-    
+                // Attach an event listener to each song
+                Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+                    e.addEventListener("click", ()=>{
+                        currentSong = e.querySelector(".songinfo").getElementsByTagName("div")[0].innerHTML
+                        console.log(e.querySelector(".songinfo").getElementsByTagName("div")[0].innerHTML);
+                        playMusic(e.querySelector(".songinfo").getElementsByTagName("div")[0].innerHTML.trim())
+                    })
+                })
 
-    audio.addEventListener("loadeddata", () => {
-        console.log(audio.duration, audio.currentSrc ,audio.currentTime,);
+                // Attach an event listener to play, previous and next button 
+                play.addEventListener("click", ()=>{
+                    if(currTrack.paused){
+                        currTrack.play()
+                        play.src = "img/pause.svg"
+                        
+                    }else{
+                        currTrack.pause()
+                        play.src = "img/play.svg"
+                }
+                })
+
+                currTrack.addEventListener("timeupdate", ()=>{
+                    console.log(Math.floor(currTrack.currentTime, currTrack.duration));
+                    document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currTrack.currentTime)} / ${secondsToMinutesSeconds(currTrack.duration)}`   
+                    
+                })
         
   
-});
 }
 
 main()
-
 
 
 
